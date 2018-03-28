63193910a05fdcce18707e622e8c75d57871d3e8
Yu Chen <yu_ch@126.com>
2018-03-29 01:09:30 +0800

    测试版本已经上线
    运行run可开始程序，启动flask。
    可以接受index页面的手动提交，也可接受get方式的直接提交

--- mail.py
+++ mail.py
@@ -9,18 +9,17 @@ password="abcd****"
 receiver="48407273@qq.com"
 sender="yu_ch@126.com"
 
-subject="Test  Mail from Python"
+#sendmal(title,content)
 
+def sendmail(title="Test  Mail from Python",content="Hello Python SMTP Mail"):
+    msg=MIMEText(content)
+    msg['Subject']=Header(title,'utf-8')
+    msg['From']=sender
+    msg['To']=receiver
+    smtp=smtplib.SMTP(smtpserver)
+    smtp.login(username,password)
+    smtp.sendmail(sender,receiver,msg.as_string())
+    smtp.quit()
 
-msg=MIMEText("Text")
-msg['Subject']=Header(subject,'utf-8')
-msg['From']=sender
-msg['To']=receiver
-
-print(msg.as_string())
-smtp=smtplib.SMTP(smtpserver)
-
-smtp.login(username,password)
-smtp.sendmail(sender,receiver,msg.as_string())
-
-smtp.quit()
+if __name__ == "__main__":
+    sendmail()
---
+++ run.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask import request
+from flask import url_for
+from flask import render_template
+import mail
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def show_hello():
+    return "<h1>Just is a Test Webpage."
+@app.route('/var/<var>')
+def show_var(var):
+    return "<h1>Test var In Flask:var = %s"%var
+@app.route('/url/<url>')
+def show_url(url):
+    return "<h1>URL For %s is %s"%(url,url_for(url))
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+#post title and content to /notify
+@app.route('/notify')
+def notify():
+    title=request.args["title"]
+    content=request.args["content"]
+    print("title:%s/n/rcontent:%s"%(title,content))
+    try:
+        mail.sendmail(title,content)
+    except Exception as e:
+        return "<h1>Exception accured while sending mail:<br><h2><font color='red'>%s</font>"%e
+    return "<h1><font  color='green'>Success sending mail notify</font>."
+
+if __name__ == "__main__":
+    app.run()
